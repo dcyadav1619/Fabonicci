1f0baa764b3fa348e3933cdc790060996ebc4696
DINESH  CHANDRA <90493714+dcyadav1619@users.noreply.github.com>
2021-11-11 18:23:27 +0530

    Add files via upload

---
+++ Fibonacci.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Write a Python program to get the Fibonacci series between 0 to 50
+
+# In[23]:
+
+
+num1 = 0
+num2 = 1
+while num2 < 50 :
+    print(num2)
+    num = num1+num2
+    num1 = num2
+    num2 = num
+
